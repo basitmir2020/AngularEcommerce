324015b1af4d6b5dbba94ea29096a21cb7ec988e
basitmir2020 <xaid902@gmail.com>
2023-02-27 23:16:10 +0530

    Added Header And Footer Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
 
+  
+    HeaderComponent,
+        FooterComponent
   ],
   imports: [
     BrowserModule
